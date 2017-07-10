fd265dee23bb075b3085d40945b0d60a13d45260
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-10 17:56:35 +0300

    Implemented sub sum in Array processing Tool

--- secondTask(JavaScript)/ArrayProcessingTool.js
+++ secondTask(JavaScript)/ArrayProcessingTool.js
@@ -1,8 +1,17 @@
 var ArrayProcessingTool = (function() {
     return {
          getMaxSubSum: function (array) {
-                 
-         }       
+                 var maxSum = 0;
+                 var sum = 0;
+                 for (var i = 0; i < array.length; i++) {
+                     sum += array[i];
+                     if (maxSum < sum) {
+                         maxSum = sum;
+                     } else if (sum < 0) {
+                         sum = 0;
+                     }
+                 }
+                 return  maxSum;
+         } 
     }
-
 })();
