820f8dcbde9b9619e5f135d01cc4cb1ef7bde898
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-11 10:02:00 +0300

    Implemented 1.Array Processing Tool without O(n^2).

--- secondTask(JavaScript)/ArrayProcessingTool.js
+++ secondTask(JavaScript)/ArrayProcessingTool.js
@@ -12,6 +12,44 @@ var ArrayProcessingTool = (function() {
                      }
                  }
                  return  maxSum;
-         } 
+         },
+
+         getMin: function (array) {
+             return Math.min.apply(this, array);
+         },
+
+         getMax: function (array) {
+             return Math.max.apply(this, array);
+         }, 
+
+         getMedium: function (array) {
+            array.sort(function(a,b) { return a - b });
+            var index = parseInt(array.length / 2);
+            if (array.length % 2 == 0)
+            {
+                return (array[index] + array[index - 1 ]) / 2;
+            } else {
+                return (array[index]);
+            }
+        },
+
+        getMaxIcrSeq: function (array) {
+            var sequence = [];
+            var maxSequence = [];
+            sequence.push(array[0]);
+            for (var i = 1; i < array.length + 1; i++) {
+                if (array[i] > array[i-1])
+                {
+                    sequence.push(array[i]);
+                } else  {
+                    if (maxSequence.length < sequence.length) {
+                        maxSequence = sequence;
+                    } 
+                    sequence=[];
+                    sequence.push(array[i]);
+                }
+            }
+            return maxSequence;
+        }
     }
 })();
