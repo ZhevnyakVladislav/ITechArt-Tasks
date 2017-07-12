bbcaa8e7583c5375bf688bffe5e42376542b6863
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-12 15:19:13 +0300

    Implemented Text Formatter

--- secondTask(JavaScript)/TextFormatter.js
+++ secondTask(JavaScript)/TextFormatter.js
@@ -1,30 +1,32 @@
 (function () {
 
     function endOfWord(string, i) {
-        while(string[i] != (' ' || '!' || '.' || '?')) {
+        while(!string[i].match(/[\s\.\?\.]/)) {
             i++;
         }
         return i;
     }
 
     function endOfSentence(string, i) {
-        while(string[i] != '.')  {
+        while(!string[i].match(/[\!\?\.]/))  {
             i++;
         }
         return i;
     }
 
-    function endOfText(string, length) { 
-        if (length < string.length) {
-            return true;
-        }
-
-    }
 
     function lineBreak(string, separationStart, separationEnd, element) {
         var start = string.substring(0, separationStart);
         var end = string.substring(separationEnd);
-        string = start + element + end;
+        string = start.trim() + element + end;
+        return string;
+    }
+
+    function endOfText(string, rows, space, length) {
+        if (space + length > rows * length) {
+            space = endOfWord(string,space + length);
+            string = string.split('',space).concat('...').join('');
+        }
         return string;
     }
 
@@ -32,19 +34,22 @@
         var element = '\n';
         var space = 0;
         var i = 1;
-        while (space + length < string.length) {
+        while (space + length < string.length && i < rows) {
             space = endOfWord(string, space + length);
             string = lineBreak(string, space, space + 1, element);
+            i++;
         }
-        return string;
+        return endOfText(string, rows, space, length);
     }
 
     function onSentence(string, length, rows) {
         var element = '\n';
         var space = 0;
-        while (space + 2 < string.length) {
+        var i = 1;
+        while (space + 2 < string.length && i < rows) {
             space = endOfSentence(string, space + 1);
             string = lineBreak(string, space + 1, space + 1, element);
+            i++;
         }
         return string;
     } 
@@ -52,28 +57,36 @@
     function onSymbol(string, length, rows) {
         var space = 0;
         var i = 1;
-        while (length * i < string.length) {
+        while (length * i < string.length && i < rows) {
             var element = '\n';
             if (!!string[length * i].match(/[a-zA-Zа-я]/)) {
                 element='-\n';
+                string = lineBreak(string, length * i, length * i, element);
             } else if (string[length * i].match(/\s/)) {
-                element='---\n'
+                string = lineBreak(string, length * i, length * i + 1, element);
+            } else if (string[length * i].match(/[[\!\?\.]/)) {
+                string = lineBreak(string, length * i + 1, length * i, element);
             }
-            var start = string.substring(0, length * i);
-            var end = string.substring(length * i);
-            string = start + element + end;
             i++;
+        }
+        if (string.length > rows * length) {
+                    space = endOfWord(string, length*rows);
+                    string = string.split('',space).concat('...').join('');
         }
         return string;
     }
 
-    function TextFormatter(string, type, length=60, rows=999) {
+    function TextFormatter(string, type, length=60, rows) {
+        if (!rows) {
+            rows = Math.ceil(string.length / length);
+        }
         switch(type) {
             case 'onWord': { return onWord(string, length, rows)};
             case 'onSymbol': { return onSymbol(string, length, rows)};
             case 'onSentence': { return onSentence(string, length, rows)};
+            case 'none': { return string};
+            
         }
     }
-
     window.TextFormatter = TextFormatter;
 })();
