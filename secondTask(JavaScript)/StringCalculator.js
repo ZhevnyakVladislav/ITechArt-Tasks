a4c87aab7d2d0ea113e67514999f095c31109e65
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-13 16:37:50 +0300

    Added feature to String Calculator

--- secondTask(JavaScript)/StringCalculator.js
+++ secondTask(JavaScript)/StringCalculator.js
@@ -1,23 +1,24 @@
 var StringCalculator = (function() {
     return {
         addition: function(a, b) {
-            return Number(a) + Number(b);
+            console.log();
+            return Number(a.replace(/,/, '.')) + Number(b.replace(/,/, '.'));
         },
 
         substraction: function(a, b) {
-            return Number(a) - Number(b);
+            return Number(a.replace(/,/, '.')) - Number(b.replace(/,/, '.'));
         },
 
         multiplication: function(a, b) {
-            return Number(a) * Number(b);
+            return Number(a.replace(/,/, '.')) * Number(b.replace(/,/, '.'));
         },
 
         division: function(a, b) {
-            return Number(a) / Number(b);
+            return Number(a.replace(/,/, '.')) / Number(b.replace(/,/, '.'));
         },
 
         equality: function(a, b) {
-            if (Number(a) == Number(b)) {
+            if (Number(a.replace(/,/, '.')) == Number(b.replace(/,/, '.'))) {
                 return true;
             }
             return false;
