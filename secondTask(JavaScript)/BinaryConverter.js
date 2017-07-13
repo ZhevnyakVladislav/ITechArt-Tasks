101576f9bdbd41dd88e43cdc2f912723f6e241ee
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-13 18:00:49 +0300

    Fixed method from2To10

--- secondTask(JavaScript)/BinaryConverter.js
+++ secondTask(JavaScript)/BinaryConverter.js
@@ -1,11 +1,32 @@
 var BinaryConverter = (function () {
+
+    
+    function toSystem(value, system) {
+        var a = 1;
+        while(a > 1) {
+            a = value % system;
+            if (a = 1);
+
+        }
+    }
+
     return {
         from2To10: function(array) {
-            var number = array.join('');
-            return parseInt(number, 2);
+            var value = 0;
+            for (var i = 0; i < array.length ; i++) {
+                if (array[i] == 1) {
+                    value += Math.pow(2, i);
+                }
+            }
+            return value;
         },
-        from10To2: function (number) {
-            return parseInt(number).toString(2).split('');
+        from10To2: function (array) {
+            
+            var value = 0;
+
+            console.log(15/2);
+            
+            return true;
         }
     }    
 })();
