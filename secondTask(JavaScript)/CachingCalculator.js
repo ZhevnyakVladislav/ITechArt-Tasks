50e21909d0b370ce9d311dd5036138556d521de9
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-13 16:18:32 +0300

    Implemented Caching Calculator

--- secondTask(JavaScript)/CachingCalculator.js
+++ secondTask(JavaScript)/CachingCalculator.js
@@ -1,5 +1,21 @@
-var CachingCalculator = (function () {
-    return {
-        
+(function() {
+    
+    function CachingCalculator(func) {
+        var lastA, lastB, value;
+        return function(a, b) {
+            if(a==lastA && b == lastB)
+            {
+                console.log('cache');
+                return CachingCalculator.cache;
+            }
+            CachingCalculator.cache = func(a,b);
+            lastA = a;
+            lastB = b;
+            return CachingCalculator.cache;
+        };
     }
+
+
+    window.CachingCalculator = CachingCalculator;
+
 })();
