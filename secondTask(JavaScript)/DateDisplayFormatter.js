e607a51e155e33aef4fc50512c80f0107bed8321
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-13 13:09:33 +0300

    Some function in Data Display Formatter

--- secondTask(JavaScript)/DateDisplayFormatter.js
+++ secondTask(JavaScript)/DateDisplayFormatter.js
@@ -1,7 +1,47 @@
 var  DateDisplayFormatter = (function() {
+
+    function parseDate(string) {
+        var day = string.substring(0,2);
+        var month = string.substring(2,4);
+        var year = string.substring(4);
+        return { day: day, month: month, year: year};
+    }
+
+    function createDate(date) {
+        return date.day.concat('-', date.month,'-', date.year);
+    }
+
+    function parseDateByReg(string,regular) {
+        var year = string.substr(regular.indexOf('YYYY'),4);
+        var month = string.substr(regular.indexOf('MM'),2);
+        var day = string.substr(regular.indexOf('DD'),2);
+        return { day: day, month: month, year: year};
+    }
+
+    function getStringMonth(date) {
+        var tmp = new Date(date.year, date.month, date.day);
+        return tmp.toDateString().split(' ')[1];
+    }
+
     return {
-        getDateFromTicks: function(string) {
-            
+        toNumber: function(string) {
+            return createDate(parseDate(string));
+        },
+
+        toString: function(string) {
+            var date = parseDate(string);
+            date.month = getStringMonth(date);
+            return createDate(date);
+        },
+
+        regToNumber: function(string, regular) {
+            return createDate(parseDateByReg(string,regular));
+        },
+
+        regToString: function(string, regular) {
+            var date = parseDateByReg(string,regular);
+            date.month = getStringMonth(date);
+            return createDate(date);
         }
     }
 })();
