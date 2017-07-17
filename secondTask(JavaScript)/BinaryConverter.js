486a7c2638d2d011adcb2a6c18bbd51958b6cbc2
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-17 17:33:25 +0300

    Fixed BinaryConverter

--- secondTask(JavaScript)/BinaryConverter.js
+++ secondTask(JavaScript)/BinaryConverter.js
@@ -1,64 +1,27 @@
-// REVIEW: оно работает, но можно сделать гораздо проще, причем не важно в систему с каким основанием переводить
-
 var BinaryConverter = (function () {
 
-
-    function to2System(number) {
+    function convertToNumber(value, from) {
+        var result = 0;
+        for (var i = 0; i < value.length; i++) {
+            result += value[i] * Math.pow(from,i);
+        }
+        return result;
+    }
+    
+    function convertToEnd(value, to) {
         var array = [];
-        value = number;
-        do {
-            // REVIEW: var надо
-            a = value / 2;
-            value = parseInt(a);
-            array.push( a == value ? 0 : 1);
-        } while(a >= 2)
-        array.push(Math.floor(a));
+        while( value > to ) {
+            var a = value % to;
+            array.push(a);
+            value = Math.floor(value / to);
+        }
+        array.push(value);
         return array;
     }
-
-    function binaryArrSum(a, b) {
-        var length = a.length > b.length ? a.length : b.length;
-        var value = [];
-        var add = 0;
-        for (var  i = 0; i < length; i++) {
-            a[i] = isNaN(a[i]) ? 0 : a[i];
-            b[i] = isNaN(b[i]) ? 0 : b[i];
-            var curr = a[i] + b[i] + add;
-            if (curr == 2) {
-                value.push(0);
-                // REVIEW: var не надо
-                var add = 1;
-            } else if (curr == 3) {
-                value.push(1);
-                add = 1;
-            } else {
-                value.push(curr);
-                add = 0;
-            }
+    return {
+        convert: function(value, from, to) {
+            return convertToEnd(convertToNumber(value,from),to);
         }
-        value.push(add);
-        return value;
     }
 
-    return {
-        from2To10: function(array) {
-            var value = 0;
-            for (var i = 0; i < array.length ; i++) {
-                if (array[i] == 1) {
-                    value += Math.pow(2, i);
-                }
-            }
-            return value;
-        },
-        
-        from10To2: function (array) {
-            var numberArr = [];
-            // REVIEW: var надо
-            value = [0];
-            for (var i = 0; i < array.length; i++) {
-                value = binaryArrSum(value, to2System(array[i] * Math.pow(10,i)));
-            }
-            return parseInt(value.reverse().join(''));
-        }
-    }    
 })();
