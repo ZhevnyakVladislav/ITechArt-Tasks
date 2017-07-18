64c2040570b50a066e8e16935b5a32b2afdf04b3
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-18 10:51:40 +0300

    Changed the logic of StringCalculator

--- secondTask(JavaScript)/StringCalculator.js
+++ secondTask(JavaScript)/StringCalculator.js
@@ -6,23 +6,39 @@ var StringCalculator = (function() {
         } 
         return Number(value);
     }
+
+    function convertNumber(a, b, operand) {
+        var length1 = a.toString().split(/[\.|,]/)[1];
+        var length2 = b.toString().split(/[\.|,]/)[1];
+        length1 = (typeof length1 == 'undefined') ? 0 : length1.length;
+        length2 = (typeof length2 == 'undefined') ? 0 : length2.length;
+        var length = Math.max(length1,length2);
+        a = replace(a) * Math.pow(10,length);
+        b = replace(b) * Math.pow(10,length);
+        if (operand == '*') {
+            return (eval(a + operand + b)) / Math.pow(10, length + length);
+        } else if (operand == '/') {
+            return (eval(a + operand + b)) / Math.pow(10, length - length);
+        }
+        return (eval(a + operand + b)) / Math.pow(10,length);
+    }
+
     return {
         
         addition: function(a, b) {
-            console.log();
-            return replace(a) + replace(b);
+            return convertNumber(a, b, '+');
         },
 
         substraction: function(a, b) {
-            return replace(a) - replace(b);
+            return convertNumber(a, b, '-');
         },
 
         multiplication: function(a, b) {
-            return replace(a) * replace(b);
+            return convertNumber(a, b, '*');
         },
 
         division: function(a, b) {
-            return replace(a) / replace(b);
+            return convertNumber(a, b, '/') ;
         },
 
         equality: function(a, b) {
