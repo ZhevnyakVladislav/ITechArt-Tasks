2d50d6b857d7eb02da328f4072e6d6c45af4f796
Stanislav Tarazevich <stanislav.tarazevich@itechart-group.com>
2017-07-19 17:39:18 +0300

    review

--- thirdTask(JavaScript)/PartialApplication.js
+++ thirdTask(JavaScript)/PartialApplication.js
@@ -1,9 +1,11 @@
 (function(){
     function PartialApplication() {
         var context = this;
+        // var
         params = Array.prototype.slice.call(arguments);
         var funct = params.pop();
         return function() {
+            // var
             params2 = Array.prototype.slice.call(arguments);
             var newParams = params.concat(params2);
             return funct.apply(context,newParams);
