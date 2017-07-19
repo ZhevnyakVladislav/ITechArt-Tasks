9fe451ce9ddbe24eda92ddfaf81da5f0d85a1884
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-19 18:10:48 +0300

    Implemented Map

--- thirdTask(JavaScript)/Map.js
+++ thirdTask(JavaScript)/Map.js
@@ -1,7 +1,15 @@
 (function(){
-
-    Array.prototype.myMap = function(){
-        console.log(this);
+    Array.prototype.myMap =  function(func) {
+        if (typeof func != "function" ) {
+            return this;
+        }
+        var newArray = [];
+        for (var i = 0; i < this.length; i++) {
+            var element = func(this[i], i, this);
+            if(element) {
+                newArray.push(element);
+            }
+        }
+        return newArray;
     }
-    
 })();
