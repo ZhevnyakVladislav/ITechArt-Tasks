aff53038375710691fb9567df94a5b69e98e51c5
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-19 17:43:56 +0300

    Fixed

--- thirdTask(JavaScript)/PartialApplication.js
+++ thirdTask(JavaScript)/PartialApplication.js
@@ -1,15 +1,13 @@
 (function(){
-    function PartialApplication() {
+    function partialApplication() {
         var context = this;
-        // var
-        params = Array.prototype.slice.call(arguments);
+        var params = Array.prototype.slice.call(arguments);
         var funct = params.pop();
         return function() {
-            // var
-            params2 = Array.prototype.slice.call(arguments);
+            var params2 = Array.prototype.slice.call(arguments);
             var newParams = params.concat(params2);
             return funct.apply(context,newParams);
         }
     }
-    window.PartialApplication = PartialApplication;
+    window.partialApplication = partialApplication;
 })();
