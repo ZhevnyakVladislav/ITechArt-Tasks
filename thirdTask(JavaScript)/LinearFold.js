6685d65544829fb0cef8161538eea40453c624e4
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-20 12:23:02 +0300

    Implemented LinearFold

---
+++ thirdTask(JavaScript)/LinearFold.js
@@ -0,0 +1,18 @@
+;(function() {
+    function linearFold(array, funct, initialVlue) {
+        if(typeof array != "array" && typeof funct != "function") {
+            throw 'error parameters';   
+        } 
+        var previousValue = initialVlue;
+        var start = 0;
+         if(!initialVlue) {
+            start = 1;
+            previousValue = array[0];
+        }
+        for (var i = start; i < array.length; i++) {
+            previousValue = funct(previousValue, array[i], i, array);
+        }
+        return previousValue;
+    }
+    window.linearFold = linearFold;
+})();
