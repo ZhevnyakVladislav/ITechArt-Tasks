6c86c87d23b2dcafa351c813997353bb2d82abe6
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-20 14:28:27 +0300

    Implemented LazyEvaluation

---
+++ thirdTask(JavaScript)/LazyEvaluation.js
@@ -0,0 +1,9 @@
+;(function() {
+    function lazyEvaluation(funct) {
+        var params = Array.prototype.slice.call(arguments,1);
+        return function () {
+            return funct.apply(this, params);
+        }
+    }
+    window.lazyEvaluation = lazyEvaluation;
+})();
