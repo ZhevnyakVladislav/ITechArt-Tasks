49a5cf7cad5fe2cb616a6e051ea594d55c1e4d11
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-20 14:07:54 +0300

    Implemented First

---
+++ thirdTask(JavaScript)/First.js
@@ -0,0 +1,12 @@
+;(function() {
+    Array.prototype.firstByCondition = function(funct) {
+        if(typeof funct != 'function') {
+            throw '';
+        }
+        for (var i = 0; i < this.length; i++) {
+            if(funct(this[i], i, this)) {
+                return { obj: this[i], index: i };
+            }
+        }
+    }
+})();
