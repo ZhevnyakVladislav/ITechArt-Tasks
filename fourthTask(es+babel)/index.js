d1912d510c4501926e905e1251ab0c426f46a620
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-07-24 16:49:17 +0300

    Implemented on es6

---
+++ fourthTask(es+babel)/index.js
@@ -0,0 +1,10 @@
+// import BinaryConverter from 'BinaryConverter.js';
+// import CachingCalculator from'CachingCalculator.js';
+// import PartialApplication from'PartialApplication.js';
+// import LinearFold from'LinearFold.js';
+import LazyEvaluation from'./LazyEvaluation';
+
+(function(){
+    console.log(23);
+    window.lazyEvaluation = LazyEvaluation;
+})();
